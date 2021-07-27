d5138a9340b4f3873f58524310e7c3f1c2be3ae6
Andrzej Henek <ahenek@future-processing.com>
2021-07-27 22:34:12 +0200

    Dodano bibliotekę klas odpowiedzialną za dostep do danych i skonfigurowano połączenie z bazą danych

--- crm/Startup.cs
+++ crm/Startup.cs
@@ -1,13 +1,10 @@
+using crm.DataAccess.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace crm
 {
@@ -23,6 +20,9 @@ namespace crm
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options => 
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
         }
 
